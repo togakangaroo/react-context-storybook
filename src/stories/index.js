151c4d4fddfc89c6fd373fe82ffdcee950841374
George Mauer <gmauer@gmail.com>
2018-10-17 23:40:50 -0500

    cleanup

--- src/stories/index.js
+++ src/stories/index.js
@@ -40,9 +40,14 @@ const StarRatingContainer = styled.div`
 `
 
 const HiddenInput = styled.input`
-    visibility: hidden;
-    height: 0;
-    width: 0;
+    border:0 none;
+    clip:rect(0px,0px,0px,0px);
+    height:1px;
+    margin:-1px;
+    overflow:hidden;
+    padding:0;
+    position:absolute;
+    width:1px
 `
 const StarRatingInput = ({rating, starCount=5}) => (
     <HiddenInput type="range" min={0} max={starCount} value={rating} readOnly={true} />
@@ -279,118 +284,105 @@ console.log(b)
         </>
     ))
 
-    .add(`Dynamic Scoping`, () => (
-        <>
-          <Markdown source={`
-# Dynamic Scoping
+    .add(`Dynamic Scoping`, () => render(
+        md(`
+            # Dynamic Scoping
 
-* Rarer
-* Can make things incredibly extensible
-                `}/>
-          <figure style={{display: `flex`, alignItems: `flex-start`}}>
-            <div style={{flex: `1`}}>
-                <Highlight className="jsx">{`
-let a = 1
-let b = 2
+            * Rarer
+            * Can make things incredibly extensible
+        `),
+        <figure style={{display: `flex`, alignItems: `flex-start`}}>
+            <Growing>
+              {code(`
+                    let a = 1
+                    let b = 2
 
-const fn2 = () => {
-    a = 3
-}
+                    const fn2 = () => {
+                        a = 3
+                    }
 
-const fn1 = (a) => {
-    a = 4
-    b = 3
-    fn1()
-}
+                    const fn1 = (a) => {
+                        a = 4
+                        b = 3
+                        fn1()
+                    }
 
-fn2(a)
+              fn2(a)
 
-console.log(a)
-console.log(b)
-                    `}</Highlight>
-                </div>
-                <img src="/dynamic_scoping.png" style={{width: `40%`, objectFit: 'contain'}} />
-            </figure>
-        </>
+              console.log(a)
+              console.log(b)
+              `)}
+            </Growing>
+            <ShrinkingImg src="/dynamic_scoping.png" style={{width: `40%`}} />
+        </figure>
     ))
 
-    .add(`Opt-in Dynamic Scoping`, () => (
-        <>
-          <div style={{display: `flex`, flexDirection: `column`}}>
-            <Markdown source={`
-# Dynamic Scoping is Confusing
+    .add(`Opt-in Dynamic Scoping`, () => render(
+        <Stack>
+            {md(`
+                # Dynamic Scoping is Confusing
 
-But Opt-In dynamic scoping...is awesome
-                `}/>
-            <img src="trex-samurai.jpg" style={{flex: `1`, height: `-webkit-fill-available`}} />
-         </div>
-        </>
+                But Opt-In dynamic scoping...is _awesome_
+            `)}
+             <GrowingImg src="trex-samurai.jpg" />
+        </Stack>
     ))
-
 
     .add(`Context With Api`, () => {
         const ApiContext = React.createContext(null)
-        return (
-        <>
-          <Markdown source={`
-# Create a React Context
-                `}/>
-          <Highlight className="jsx">{`
-const ApiContext = React.createContext(null)
-                `}</Highlight>
-          <Markdown source={`
-## Usage
-                `}/>
-          <Highlight className="jsx">
-            {`
-<ApiContext.Provider value={{getReviews: () => Promise.resolve(stubReview())}}>
-    <ApiContext.Consumer>{(api) =>
-      <ApiLoadedReviewSummary api={api} id={123} />
-    }</ApiContext.Consumer>
-</ApiContext.Provider>
-                `}
-          </Highlight>
-
-          <ApiContext.Provider value={{getReviews: () => Promise.resolve(stubReview())}}>
-            <ApiContext.Consumer>{(api) =>
-              <ApiLoadedReviewSummary api={api} id={123} />
-            }</ApiContext.Consumer>
-          </ApiContext.Provider>
-        </>
-    )})
+        return render(
+            md(`
+                # Create a React Context
+            `),
+            code(`
+                const ApiContext = React.createContext(null)
+            `),
+            md(`
+                ## Usage
+            `),
+            code(`
+                <ApiContext.Provider value={{getReviews: () => Promise.resolve(stubReview())}}>
+                    <ApiContext.Consumer>{(api) =>
+                        <ApiLoadedReviewSummary api={api} id={123} />
+                    }</ApiContext.Consumer>
+                </ApiContext.Provider>
+            `),
+            <ApiContext.Provider value={{getReviews: () => Promise.resolve(stubReview())}}>
+                <ApiContext.Consumer>{(api) =>
+                    <ApiLoadedReviewSummary api={api} id={123} />
+                }</ApiContext.Consumer>
+            </ApiContext.Provider>
+        )
+    })
 
     .add(`Without providers`, () => {
         const ApiContext = React.createContext({
             getReviews: () => Promise.resolve(stubReview())
         })
-        return (
-        <>
-          <Markdown source={`
-# Setting Providers is a Pain
+        return render(
+            md(`
+                # Setting Providers is a Pain
 
-So provide a default value instead.
-                `}/>
-          <Highlight className="jsx">{`
-const ApiContext = React.createContext({
-    getReviews: Promise.resolve(stubReview())
-})
-                `}</Highlight>
-          <Markdown source={`
-## Usage
-                `}/>
-          <Highlight className="jsx">
-            {`
-<ApiContext.Consumer>{(api) =>
-    <ApiLoadedReviewSummary api={api} id={123} />
-}</ApiContext.Consumer>
-                `}
-          </Highlight>
-
+                So provide a default value instead.
+            `),
+            code(`
+                const ApiContext = React.createContext({
+                    getReviews: Promise.resolve(stubReview())
+                })
+            `),
+            md(`
+                ## Usage
+            `),
+            code(`
+                <ApiContext.Consumer>{(api) =>
+                    <ApiLoadedReviewSummary api={api} id={123} />
+                }</ApiContext.Consumer>
+            `),
           <ApiContext.Consumer>{(api) =>
             <ApiLoadedReviewSummary api={api} id={123} />
           }</ApiContext.Consumer>
-        </>
-    )})
+        )
+    })
 
     .add(`Create HoC`, () => {
         const ApiContext = React.createContext({
@@ -403,33 +395,29 @@ const ApiContext = React.createContext({
         )
         const LoadedReviewSummary = withApiContext(ApiLoadedReviewSummary)
 
-        return (
-            <>
-              <Markdown source={`
-# Tip: Create an ApiContext Higher Order Component (HoC)
-                `}/>
-              <Highlight className="jsx">{`
-const withApiContext = (Component) => (props) => (
-    <ApiContext.Consumer>{(api) =>
-        <Component api={api} {...props} />
-    }</ApiContext.Consumer>
-)
-                `}</Highlight>
-              <Highlight className="jsx">{`
-const LoadedReviewSummary = withApiContext(ApiLoadedReviewSummary)
-                `}</Highlight>
-              <Markdown source={`
-Can now be used transparently
-                `}/>
-              <Highlight className="jsx">
-                {`
-<LoadedReviewSummary id={456} />
-                `}
-              </Highlight>
-
-              <LoadedReviewSummary id={456} />
-            </>
-        )})
+        return render(
+            md(`
+                # Tip: Create an ApiContext Higher Order Component (HoC)
+            `),
+            code(`
+                const withApiContext = (Component) => (props) => (
+                    <ApiContext.Consumer>{(api) =>
+                        <Component api={api} {...props} />
+                    }</ApiContext.Consumer>
+                )
+            `),
+            code(`
+                const LoadedReviewSummary = withApiContext(ApiLoadedReviewSummary)
+            `),
+            md(`
+                Can now be used transparently
+            `),
+            code(`
+                <LoadedReviewSummary id={456} />
+            `),
+            <LoadedReviewSummary id={456} />
+        )
+    })
 
 
     .add(`Delayed Promise Resolver`, () => {
@@ -443,81 +431,75 @@ Can now be used transparently
         )
         const LoadedReviewSummary = withApiContext(ApiLoadedReviewSummary)
 
-        return (
-            <>
-              <Markdown source={`
-# Tip: Create a Promise Reolver You Can Control
+        return render(
+            md(`
+                # Tip: Create a Promise Reolver You Can Control
 
-To test what what it looks like while loading
-                `}/>
-              <Highlight className="jsx">{`
-const resolveIn = ms => value => new Promise(resolve =>
-    setTimeout(() => resolve(value), ms)
-)
-const ApiContext = React.createContext({
-    getReviews: () => resolveIn(number("Loading delay", 5000))(stubReview())
-})
-                `}</Highlight>
-              <Highlight className="jsx">
-                {`
-<LoadedReviewSummary id={456} />
-                `}
-              </Highlight>
+                To test what what it looks like while loading
+            `),
+            code(`
+                const resolveIn = ms => value => new Promise(resolve =>
+                    setTimeout(() => resolve(value), ms)
+                )
+                const ApiContext = React.createContext({
+                    getReviews: () => resolveIn(number("Loading delay", 5000))(stubReview())
+                })
+            `),
+            code(`
+                <LoadedReviewSummary id={456} />
+            `),
+            <LoadedReviewSummary id={456} />
+        )
+    })
 
-              <LoadedReviewSummary id={456} />
-            </>
-        )})
+    .add(`Maybe not Jsx`, () => render(
+        md(`
+            # Tip: You don't *always* need Jsx
 
-    .add(`Maybe not Jsx`, () => (
-        <>
-          <Markdown source={`
-# Tip: You don't *always* need Jsx
+            Some things are significantly easier without it
+        `),
+        code(`
+            const ActiveStar = styled.i\`
+                color: gold;
+            \`
+            const InactiveStar = styled.i\`
+                color: gray;
+            \`
 
-Some things are significantly easier without it
-                `}/>
+            const Star = ({active}) => create(active ? ActiveStar : InactiveStar, {}, \`★\`)
 
-          <Highlight className="javascript">
-            {`
-const ActiveStar = styled.i\`
-    color: gold;
-\`
-const InactiveStar = styled.i\`
-    color: gray;
-\`
+            const StarRatingContainer = styled.div\`
+                font-size: 50px;
+                display: flex;
+                align-items: center;
+            \`
+            const HiddenInput = styled.input\`
+                border:0 none;
+                clip:rect(0px,0px,0px,0px);
+                height:1px;
+                margin:-1px;
+                overflow:hidden;
+                padding:0;
+                position:absolute;
+                width:1px
+            \`
+            const StarRatingInput = ({rating, starCount=5}) => (
+                <HiddenInput type="range" min={0} max={starCount} value={rating} readOnly={true} />
+            )
 
-const Star = ({active}) => create(active ? ActiveStar : InactiveStar, {}, \`★\`)
+            const create = React.CreateElement
+            const StarRating = ({rating, starCount=5}) =>
+                create(StarRatingContainer, null,
+                    create(StarRatingInput, {rating, starCount}),
+                        ...repeat(starCount)(i => create(Star, {active: i < rating}))
+                )
 
-const StarRatingContainer = styled.div\`
-    font-size: 50px;
-    display: flex;
-    align-items: center;
-\`
-
-const HiddenInput = styled.input\`
-    visibility: hidden;
-    height: 0;
-    width: 0;
-\`
-const StarRatingInput = ({rating, starCount=5}) => (
-    <HiddenInput type="range" min={0} max={starCount} value={rating} readOnly={true} />
-)
-
-const create = React.CreateElement
-const StarRating = ({rating, starCount=5}) =>
-    create(StarRatingContainer, null,
-        create(StarRatingInput, {rating, starCount}),
-            ...repeat(starCount)(i => create(Star, {active: i < rating}))
-    )
-
-const ReviewSummary = ({name, rating}) => (
-    <figure>
-        <figcaption>{name}</figcaption>
-        <StarRating rating={rating} starCount={5} />
-    </figure>
-)
-          `}
-          </Highlight>
-        </>
+            const ReviewSummary = ({name, rating}) => (
+                <figure>
+                    <figcaption>{name}</figcaption>
+                    <StarRating rating={rating} starCount={5} />
+                </figure>
+        `)
     ))
 
     .add(`Auto unmount`, () => {
@@ -549,70 +531,94 @@ const ReviewSummary = ({name, rating}) => (
                   }
               }
         const LoadedReviewSummary = withApiContext(`getReviews`)(ApiLoadedReviewSummary)
-        return (
-            <>
-              <Markdown source={`
-# More Complex Things
+        return render(
+            md(`
+                # More Complex Things
 
-With our \`withApiContext\` HoC we now have hooks to do other interesting things
+                With our \`withApiContext\` HoC we now have hooks to do other interesting things
 
-* Let's force people to be explicit about what apis they want to use
-* Let's never resolve any promises if our HoC is unmounted
+                * Let's force people to be explicit about what apis they want to use
+                * Let's never resolve any promises if our HoC is unmounted
 
-Do this by creating a new api object to pass down to the component. This will only have the functions requested and
-proxy the promise with its own version that will only resolve if the component is not mounted
-`}/>
-              <Highlight className="javascript">{`
-const ApiContext = React.createContext({
-    getReviews: () => resolveIn(number("Loading delay", 5000))(stubReview())
-})
+                Do this by creating a new api object to pass down to the component. This will only have the functions requested and
+                proxy the promise with its own version that will only resolve if the component is not mounted
+            `),
+            code(`
+                const ApiContext = React.createContext({
+                    getReviews: () => resolveIn(number("Loading delay", 5000))(stubReview())
+                })
 
-const proxyPromise = (promise, {beforeCompletion}) => new Promise((resolve, reject) =>
-    promise.then(beforeCompletion(resolve), beforeCompletion(reject))
-)
-const propsToObject = (props, createValue) =>
-        Object.fromEntries(props.map(prop => [prop, (...args) => createValue(prop, ...args)]))
+                const proxyPromise = (promise, {beforeCompletion}) => new Promise((resolve, reject) =>
+                    promise.then(beforeCompletion(resolve), beforeCompletion(reject))
+                )
+                const propsToObject = (props, createValue) =>
+                        Object.fromEntries(props.map(prop => [prop, (...args) => createValue(prop, ...args)]))
 
-const withApiContext = (...apiPropNames) => (Component) =>
-        class extends React.Component {
-            render = () => (
-                <ApiContext.Consumer>{(contextApi) => {
-                    const api = propsToObject(apiPropNames,
-                    (prop, ...args) => proxyPromise(contextApi[prop](...args), {beforeCompletion: this.ifMounted})
-                    )
-                    return (
-                        <Component api={api} {...this.props} />
-                    )
-                }}</ApiContext.Consumer>
-            )
-            ifMounted = fn => (...args) => !this.isUnmounted && fn(...args)
-            componentWillUnmount = () => {
-                this.isUnmounted = true
-            }
-        }
-const LoadedReviewSummary = withApiContext("getReviews")(ApiLoadedReviewSummary)
-            `}
-            </Highlight>
-              {!boolean("currently mounted", true) ? null :
+                const withApiContext = (...apiPropNames) => (Component) =>
+                        class extends React.Component {
+                            render = () => (
+                                <ApiContext.Consumer>{(contextApi) => {
+                                    const api = propsToObject(apiPropNames,
+                                    (prop, ...args) => proxyPromise(contextApi[prop](...args), {beforeCompletion: this.ifMounted})
+                                    )
+                                    return (
+                                        <Component api={api} {...this.props} />
+                                    )
+                                }}</ApiContext.Consumer>
+                            )
+                            ifMounted = fn => (...args) => !this.isUnmounted && fn(...args)
+                            componentWillUnmount = () => {
+                                this.isUnmounted = true
+                            }
+                        }
+                const LoadedReviewSummary = withApiContext("getReviews")(ApiLoadedReviewSummary)
+            `),
+              !boolean("currently mounted", true) ? null :
                 <LoadedReviewSummary id={123} />
-              }
-            </>
         )
     })
 
-    .add(`Storybook`, () => (
-          <>
-            <Markdown source={`
-# Thoughts on [Storybook](https://storybook.js.org/)
+    .add(`Storybook`, () => render(
+        md(`
+            # Thoughts on [Storybook](https://storybook.js.org/)
 
-* Very powerful "playground" tool - REPL-Driven-Development
-* Fast to set up for standard architectures
-* Great for component gallery
-* Can do some of what TDD does
+            * Very powerful "playground" tool - REPL-Driven-Development
+            * Fast to set up for standard architectures
+            * Great for component gallery
+            * Can do some of what TDD does
 
-Also
+            **Also**
 
-* Clearly this is great
-                `}/>
-          </>
+            * Clearly this presentation is cool
+       `)
       ))
+
+const trimLead = str => {
+    const line1 = str.split('\n')[1]
+    const leadingSpace = /^\s*/.exec(line1)[0]
+    return str.replace(new RegExp(`\n${leadingSpace}`, 'g'), '\n').trim()
+}
+const md = source => create(Markdown, {source: trimLead(source)})
+const code = (source, className="javascript") => create(Highlight, {className}, trimLead(source))
+const render = (...elements) => create(React.Fragment, null, ...elements)
+
+const FlexContainer = styled.div`
+    display: flex;
+`
+const Stack = styled(FlexContainer)`
+    flex-direction: column
+`
+const AutosizeImg = styled.img`
+    height: fill-available;
+    object-fit: contain;
+    object-position: top;
+`
+const Growing = styled.div`
+    flex-grow: 1;
+`
+const GrowingImg = styled(AutosizeImg)`
+    flex-grow: 1;
+`
+const ShrinkingImg = styled(AutosizeImg)`
+    flex-shrink: 1;
+`
